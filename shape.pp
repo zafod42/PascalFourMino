66ba9bc443e359591ce7417ed89ede928af8c376
Pavel Kalabin <kalabin.pp@edu.spbstu.ru>
2025-08-03 01:36:15 +0300

    More adjusts to spawn points

--- shape.pp
+++ shape.pp
@@ -214,8 +214,8 @@ implementation
 
 procedure NewShape(var shape: TetrisShape; var bag: SevenBag);
 const
-                                                  { (J,  I,  O,  L,  Z,  T,  S); }
-    posYStart: array [1..SHAPESCOUNT] of integer = (-1, -2, -2, -1, -1, -1, -1);
+                                                  {(J,  I,  O, L, Z, T, S); }
+    posYStart: array [1..SHAPESCOUNT] of integer = (0, -2, -2, 0, 0, 0, 0);
 var
     _type: ShapeType;
 begin
