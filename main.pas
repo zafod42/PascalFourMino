e7c34a422532474fa7b67594f9ca3e00faee8c04
Pavel Kalabin <kalabin.pp@edu.spbstu.ru>
2025-08-03 01:36:37 +0300

    Fixed FPS Game Loop

--- main.pas
+++ main.pas
@@ -1,13 +1,20 @@
 program tetris;
-uses game;
+uses game, crt, sysutils;
+const 
+    FrameRate = 30;
+    FrameMS = 1000 div FrameRate;
 var
     tetrisGame: game.TetrisGame;
+    _start, _end: int64;
 begin
     TetrisInit(tetrisGame);
     repeat
+        _start := GetTickCount64;
         ProcessInput(tetrisGame);
         Update(tetrisGame);
         Render(tetrisGame);
+        _end := GetTickCount64;
+        delay(FrameMS - (_end - _start));
     until not IsGameRunning(tetrisGame);
     writeln('Game Finished');
 end.
